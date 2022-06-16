75d43e212289c601a847073983a89121e26b6e94
Exequiel65 <marcos97britos@gmail.com>
2022-06-16 16:35:29 -0300

    change server.js

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ mongoose.connect(
 );
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
 
 // Middleware
 app.use(bodyParser.json());
