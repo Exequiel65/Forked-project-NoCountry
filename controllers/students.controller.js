2742eca387a084a18514cec6aae5cc9e2c69f02b
Exequiel65 <marcos97britos@gmail.com>
2022-06-16 19:46:26 -0300

    change uploadImage

--- controllers/students.controller.js
+++ controllers/students.controller.js
@@ -73,15 +73,15 @@ const editStudents = async (req, res, next) => {
 const uploadAvatar = async (req, res, next) => {
   try {
     let response = await imgbbUploader(process.env.API_KEY_IMGBB, path.join(__dirname, `../files/${req.file.filename}`))
-    // if(response){
+    if(response){
       console.log(fs.existsSync(path.join(__dirname, '../files/' + req.file.filename)))
-      // if (fs.existsSync('./files/' + req.file.filename) && req.file.filename !== "default-image.png") {
-      //   fs.unlinkSync(`./files/${req.file.filename}`)
-      // } else {
-      //   console.log('no se encontro el archivo')
-      // }
-    // }
-    // await Students.findByIdAndUpdate(req.params.id, {avatar : response.url}, { userFindModify: false })
+      if (fs.existsSync(path.join(__dirname, '../files/' + req.file.filename)) && req.file.filename !== "default-image.png") {
+        fs.unlinkSync(path.join(__dirname,`../files/${req.file.filename}`))
+      } else {
+        console.log('no se encontro el archivo')
+      }
+    }
+    await Students.findByIdAndUpdate(req.params.id, {avatar : response.url}, { userFindModify: false })
     res.status(200).json({
       msg: "usuario actualizado",
       response
